911d3d3112805a968afb32e819501714e3d7c12b
Gustavo Lima <gustavomdcl@gmail.com>
2019-09-09 10:08:22 -0300

    Babel

--- ultimate/src/gulpTasks/javascript.js
+++ ultimate/src/gulpTasks/javascript.js
@@ -1,10 +1,16 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const concat = require('gulp-concat');
 
 function Javascript(cb) {
 
 	return gulp.src('../build/js/**/*.js')
+	.pipe(babel({
+        presets: ['@babel/preset-env']
+    }))
+    .pipe(concat('main.js'))
 	.pipe(uglify())
 	.pipe(rename({ extname: '.min.js' }))
 	.pipe(gulp.dest('../course/assets/js/'));
